513f497b5ef5532b036e94368c434e9948388874
Shaunak2003 <shaunak.karmarkar@gmail.com>
2025-05-07 22:59:57 -0400

    Backend set up
    Set up basic backend that is now using PostgreSQL data in user display
    Added gitignore file

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes';
+import authRoutes from './routes/authRoutes';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
